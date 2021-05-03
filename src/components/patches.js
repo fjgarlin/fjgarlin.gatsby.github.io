5fd92332a3e2e236c46f01828c3dd61d7f5d27f9
Fran Garcia-Linares <fjgarlin@gmail.com>
2021-05-03 12:43:51 +0200

    Refactored json structure.

--- src/components/patches.js
+++ src/components/patches.js
@@ -4,16 +4,25 @@ import PatchesData from "../data/12months12patches.json"
 const Patches = (props) => {
     return (
         <div>
-            {PatchesData.data.map((data, index) => {
-                return <div key={`content_item_${index}`}>
+            {PatchesData.data.map((data, i1) => {
+                return <div key={`content_item_${i1}`}>
                     {data.item}
                     <h3 id={"year_" + data.year}>
                         {data.year}
                         {data.blogPost && <small> - <a rel="noreferrer" href={data.blogPost} target="_blank">Blog post</a></small>}
                     </h3>
                     <ul>
-                        {data.patches.map((patch, i) => {
-                            return <li>{patch.month} : <a rel="noreferrer" href={patch.url} target="_blank">{patch.url}</a></li>
+                        {data.patches.map((patch, i2) => {
+                            return <li key={`content_li_${i2}`}>
+                                {patch.month}
+                                <ul>
+                                    {patch.urls.map((u, i3) => {
+                                        return <li key={`content_item_${i3}`}>
+                                            <a rel="noreferrer" href={u} target="_blank">{u}</a>
+                                        </li>
+                                    })}
+                                </ul>
+                            </li>
                         })}
                     </ul>
                 </div>
