7bf1ee82dd1fcfa57ce20f77562a77723880beee
Fran Garcia-Linares <fjgarlin@gmail.com>
2021-05-03 20:54:10 +0200

    Edit text.

--- src/pages/12months12patches.js
+++ src/pages/12months12patches.js
@@ -9,7 +9,7 @@ const Page12months12patches = () => {
             <h2>#12months12patches</h2>
             <p>
                 A few years ago I started this personal initiative as a way to contribute more often to Drupal (and related projects).
-                It's very easy: "try to contribute at least one patch each month of the year".
+                My goal was (and still is): "try to contribute at least one patch each month of the year".
             </p>
             <p>
                 You can read more about the challenge in the blog posts (linked below), and see the list of patches/PRs submitted over the years.
