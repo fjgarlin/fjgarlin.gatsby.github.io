6aefec74d32d0f02d4d72874dfb830d82899bf0f
Fran Garcia-Linares <fjgarlin@gmail.com>
2025-03-24 13:15:17 +0100

    Change some text.

--- src/pages/12months12patches.js
+++ src/pages/12months12patches.js
@@ -8,8 +8,8 @@ const Page12months12patches = () => {
         <Layout>
             <h2>#12months12patches</h2>
             <p>
-                In 2019, I started this personal initiative as a way to contribute more often to Drupal (and related projects).
-                My goal was (and still is): <em>"try to contribute at least one patch each month of the year"</em>.
+                In 2019, I started this personal initiative because I wanted to contribute more often to Drupal (and related projects).
+                My goal was: <em>"try to contribute at least one patch each month of the year"</em>.
             </p>
             <p>
                 I've been doing the challenge for 4 straight years, and now (2023) I work full-time at the Drupal Association, where all of my time is spent contributing to Drupal.
@@ -17,6 +17,7 @@ const Page12months12patches = () => {
             </p>
             <p>
                 You can read more about the challenge in the blog posts (linked below), and see the list of patches/PRs submitted over the years 2019-2022.
+                This was a great personal challenge that helped me grow and made me feel more involved in the community and open source.
             </p>
             <div>
                 <Patches />
--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,7 @@ const IndexPage = () => {
     return (
         <Layout>
             <h2>Hi!</h2>
-            <h3>I'm {data.site.siteMetadata.author}, a full-stack web developer specialized in maintenance of Drupal projects.</h3>
-            <p>Mostly a problem solver so throw any challenge you have at me and I'll try to help.</p>
+            <h3>I'm {data.site.siteMetadata.author}, a web developer specialized in Drupal.</h3>
             <hr/>
             <p>I work at the <a rel="noreferrer" href="https://www.drupal.org/association" target="_blank">Drupal Association</a> as part of the Engineering Team.</p>
             <p>I also like running, basketball and photography, among others.</p>
